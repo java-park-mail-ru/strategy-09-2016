b7bf2ee0f59c416826d75ec5f796261e1a1fa0ff
Victor <Kamyshin.Victor@yandex.ru>
2016-11-04 15:50:37 +0300

    rename some test

--- src/test/java/ru/mail/park/TestRunnableWithH2DB.java
+++ src/test/java/ru/mail/park/TestRunnableWithH2DB.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestRunnableApplicationH2DB {
+public class TestRunnableWithH2DB {
 
 	@Test
 	public void contextLoads() {
